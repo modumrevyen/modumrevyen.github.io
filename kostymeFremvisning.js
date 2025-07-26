824201b3fefa7de3a8c3cba32c3b3b5a74ef31d1
ShadXn <shadxn@sayver.net>
2025-07-26 14:27:38 +0200

    Update kostymeFremvisning.js

--- kostymeFremvisning.js
+++ kostymeFremvisning.js
@@ -42,7 +42,7 @@ async function loadCostumesFromSheety() {
 function addCostumeCard(c) {
   const imageId = c.imagecurl?.match(/[-\w]{25,}/)?.[0]; // Extract file ID from imageurl
   const directImageUrl = imageId
-    ? `https://drive.google.com/thumbnail?id=${imageId}`
+    ? `https://drive.google.com/thumbnail?id=${imageId}&sz=s4000`
     : "placeholder.png"; // fallback image if needed
 
   const col = document.createElement("div");
